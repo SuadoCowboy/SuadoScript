aeac8d1214f1e7740405b3e85007fd3f3d147c92
Susado <77517394+SuadoCowboy@users.noreply.github.com>
2022-04-02 16:03:00 -0300

    dah

--- plugins/example.py
+++ plugins/example.py
@@ -5,13 +5,15 @@ def init_console(_console):
     console = _console
     
     # arguments = name: str, function, is_multiple_args: bool, list_of_args: list, description: str plugin_name: str
-    console.add_command('square', create_square, False, [float, float, str], 'square <width> <height> <char=\'@\'> - draws a square', __name__)
+    console.add_command('square', create_square, False, [float, float, str], 'square <width> <height> <char> - draws a square', __name__)
 
-def create_square(width, height, char='@'):
+def create_square(width: int, height: int, char: str):
     out = ''
-    for y in range(int(height)):
-        for x in range(int(width)):
+    width = int(width)
+    height = int(height)
+    for y in range(height):
+        for x in range(width):
             out += char
-            if x == int(width)-1 and y != int(height)-1:
+            if x == width-1 and y != height-1:
                 out += '\n'
     return out
