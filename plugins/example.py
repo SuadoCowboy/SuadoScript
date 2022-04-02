d2ad78108d4de36e64d948e7e710cfe4060de79b
Susado <77517394+SuadoCowboy@users.noreply.github.com>
2022-04-02 15:04:05 -0300

    added config.cfg and example plugin

---
+++ plugins/example.py
@@ -0,0 +1,17 @@
+console = None
+
+def init_console(_console):
+    global console
+    console = _console
+    
+    # arguments = name: str, function, is_multiple_args: bool, list_of_args: list, description: str
+    console.add_command('square', create_square, False, [float, float, str], 'square <width> <height> <char=\'@\'> draws a square')
+
+def create_square(width, height, char='@'):
+    out = ''
+    for y in range(int(height)):
+        for x in range(int(width)):
+            out += char
+            if x == int(width)-1 and y != int(height)-1:
+                out += '\n'
+    return out
